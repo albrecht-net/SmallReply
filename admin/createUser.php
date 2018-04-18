7103e6cfa81e10158fd26e70dc62a11c934a0a6a
albrecht-net <jonasalbrecht9@gmail.com>
2018-04-18 21:33:53 +0200

    Formular für Benutzer- (Admin) Erfassung erstellt

---
+++ admin/createUser.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Login</title>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+	<form action="#" method="post">
+			<div>
+				<label for="username">Benutzername</label>
+				<input id="username" type="text" name="username">
+			<div>
+				<label for="firstname">Vorname</label>
+				<input id="firstname" type="text" name="firstname">	
+			<div>
+				<label for="lastname">Nachname</label>
+				<input id="lastname" type="text" name="lastname">	
+			<div>
+				<label for="email">Email</label>
+				<input id="email" type="email" name="email">
+			<div>
+				<label for="password">Passwort</label>
+				<input id="password" type="password" name="password">
+			<div>
+				<input type="submit" value="Absenden" name="submit">
+			</div>
+	</form>
+</body>
+</html>
