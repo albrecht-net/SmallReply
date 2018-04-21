6170adbf13f6763a55ad3b6220912c5e9a4319fd
albrecht-net <jonasalbrecht9@gmail.com>
2018-04-21 21:32:12 +0200

    JavaScript Verlinkung angepasst

--- admin/register.php
+++ admin/register.php
@@ -20,6 +20,8 @@ if (isset($_POST['submit'])) {
 
 	<!-- jQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
+	<!-- Bootstrap JS -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous" async></script>
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
