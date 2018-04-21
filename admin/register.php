ad79d00a4548890a57c822ec9df596c68a7dd14d
albrecht-net <jonasalbrecht9@gmail.com>
2018-04-21 21:41:18 +0200

    Meta angepasst

--- admin/register.php
+++ admin/register.php
@@ -11,7 +11,7 @@ if (isset($_POST['submit'])) {
 <html lang="de">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Registirieren</title>
 
