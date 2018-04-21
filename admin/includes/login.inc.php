beea8b27f3aa0b4c0a3fa3f3f901cb36783bd333
albrecht-net <jonasalbrecht9@gmail.com>
2018-04-21 15:03:24 +0200

    Benutzername / Passwort wird mit Datenbank abgeglichen

--- admin/includes/login.inc.php
+++ admin/includes/login.inc.php
@@ -1,3 +1,33 @@
 <?php
+// Mit der Datenbank verbinden
+include_once '../../../dbh.php';
 
+// Variablen zuweisen
+$dbTable = 'users';
+
+// Array Eingabe
+$dataInput = array(
+	'username' => $_POST['username'],
+	'password' => $_POST['password']
+);
+
+// SQL-Query bereitstellen
+$sqlresult = "SELECT * FROM `" . $dbTable . "` WHERE `username` = '" . $dataInput['username'] . "'";
+$result = mysqli_query($link, $sqlresult);
+
+// Benutzername abfragen
+if (mysqli_num_rows($result) == 0) {
+	echo date('H:i:s') . ' Der Benutzername: ' . $dataInput['username'] . ' ist ungültig.';
+	// exit();
+}
+
+// Passwort abfragen
+$dataDb = mysqli_fetch_assoc($result);
+if (!password_verify($dataInput['password'], $dataDb['password'])) {
+	echo date('H:i:s') . ' Das Passwort ist ungültig';
+	// exit();
+} else {
+	$_SESSION[''] = $dataDb[''];
+	echo date('H:i:s') . ' Erfolgreich angemeldet!';
+}
 ?>
--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,6 @@
 <?php
 // Überprüfen ob Submit geklickt wurde
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit']) && !empty($_POST['username'])) {
 	if (!include 'includes/login.inc.php') {
 		echo date('H:i:s') . ' Datei einbinden fehlgeschlagen';
 	}
