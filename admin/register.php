b396eda5268e76fd26939854304adf30c131eed8
albrecht-net <jonasalbrecht9@gmail.com>
2018-04-21 21:28:22 +0200

    Navigation bei register.php

--- admin/register.php
+++ admin/register.php
@@ -14,9 +14,42 @@ if (isset($_POST['submit'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Registirieren</title>
-	<link rel="stylesheet" href="../css/style.css">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+	<!-- jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
 </head>
 <body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#">SmallReply</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="index.html">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="records.php">Einträge</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="register.php">Neuer Benutzer<span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+		</div>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="logout.php">Abmelden</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
 	<form action="register.php" method="POST">
 			<div>
 				<label for="username">Benutzername</label>
@@ -37,8 +70,8 @@ if (isset($_POST['submit'])) {
 			<div>
 				<label for="password">Passwort</label>
 				<input id="password" type="password" name="password">
-            </div>
-            </div>
+			</div>
+			</div>
 			<div>
 				<label for="passwordRepeat">Passwort wiederholen</label>
 				<input id="passwordRepeat" type="password" name="passwordRepeat">
