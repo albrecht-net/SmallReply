8a27d6ffcf705396771d8bccbe19c7a898dd234b
albrecht-net <jonasalbrecht9@gmail.com>
2018-04-26 20:30:13 +0200

    Prüfung einer Anmeldesitzung

---
+++ admin/includes/loginSessionCheck.inc.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+// Array Sessiondata
+$dataSession = array(
+	'uid' => $_SESSION['uid'],
+	'username' => $_SESSION['username']
+);
+
+// Benutzer Session zurücksetzten
+function unsetSession() {
+	unset($_SESSION['uid']);
+	unset($_SESSION['username']);
+	header("Location: login.php");
+}
+
+// Überprüft ob User-ID oder Benutzername in der Session leer sind (leer entspricht: kein Benutzer angemeldet)
+if (empty($dataSession['uid']) || empty($dataSession['username'])) {
+	echo date('H:i:s') . ' Keine Session';
+	unsetSession();
+	exit();
+} elseif (isset($dataSession['uid']) && isset($dataSession['username'])) {
+	// Mit der Datenbank verbinden
+	include_once '../../../dbh.php';
+
+	// Variablen zuweisen
+	$dbTable = 'users';
+
+	// SQL-Query bereitstellen
+	$sqlquery = "SELECT * FROM `" . $dbTable . "` WHERE `uid` = '" . $dataSession['uid'] . "' AND `username` = '" . $dataSession['username'] . "'";
+	$query = mysqli_query($link, $sqlquery);
+
+	// Prüft ob Session mit Datenbank übereinstimmt (Wenn 1 Resultat: Benutzer vorhanden und korrekt abgeglichen)
+	if (mysqli_num_rows($query) != 1) {
+		echo date('H:i:s') . ' Benutzer abfragen fehlgeschlagen';
+		unsetSession();
+		exit();
+	}
+}
+?>
