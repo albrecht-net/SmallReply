1188b9301c78ef683b9223fe16a57a8b4364c2da
albrecht-net <jonasalbrecht9@gmail.com>
2018-05-06 18:56:23 +0200

    Query Verbesserung login.inc

--- admin/includes/login.inc.php
+++ admin/includes/login.inc.php
@@ -12,18 +12,21 @@ $dataInput = array(
 );
 
 // SQL-Query bereitstellen
-$sqlquery = "SELECT * FROM `" . $dbTable . "` WHERE `username` = '" . $dataInput['username'] . "'";
-$query = mysqli_query($link, $sqlquery);
+$sqlquery = "SELECT `username`, `password`, `uid` FROM `" . $dbTable . "` WHERE `username` = '" . $dataInput['username'] . "'";
+$result = mysqli_query($link, $sqlquery);
 
 // Benutzername abfragen
-if (mysqli_num_rows($query) == 0) {
+if (mysqli_num_rows($result) == 0) {
 	echo date('H:i:s') . ' Der Benutzername: ' . $dataInput['username'] . ' ist ungültig.';
+} else {
+    // Abfrage in Array schreiben
+    $dataDb = mysqli_fetch_assoc($result);
 }
 
-// Passwort abfragen
-$dataDb = mysqli_fetch_assoc($query);
+// Passwort validieren
 if (!password_verify($dataInput['password'], $dataDb['password'])) {
-	echo date('H:i:s') . ' Das Passwort ist ungültig';
+    echo date('H:i:s') . ' Das Passwort ist ungültig';
+    exit();
 } else {
 	$_SESSION['uid'] = $dataDb['uid'];
 	$_SESSION['username'] = $dataDb['username'];
