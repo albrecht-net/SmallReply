469960ca9742d25af58c6958a30052b9af2dc130
albrecht-net <jonasalbrecht9@gmail.com>
2018-05-17 17:42:13 +0200

    Konfiguration-Vorlage und Setup Dateien erstellt

---
+++ configSample.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    // Servername oder IP-Addresse
+    'dbServername' => '',
+
+    // Benutzername der MySQL-Datenbank
+    'dbUsername' => '',
+
+    // Passwort der MySQL-Datenbank
+    'dbPassword' => '',
+
+    // Datenbankname für SmallReply
+    'dbName' => ''
+)
+?>
