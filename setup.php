2183fd0597809c4fbcdec279a9036d33eb2d81ea
albrecht-net <jonasalbrecht9@gmail.com>
2018-05-23 21:07:17 +0200

    Formular für Benutzer Erfassung

--- setup.php
+++ setup.php
@@ -4,14 +4,14 @@ $configFilename = 'config.php';
 
 // Array Eingabe
 $dataSetup = array(
-	'step' => $_GET['step'],
-	'configTemplate' => file('configSample.php')
+    'step' => $_GET['step'],
+    'configTemplate' => file('configSample.php')
 );
 $dataInput = array(
-	'dbHost' => $_POST['dbHost'],
-	'dbUsername' => $_POST['dbUsername'],
-	'dbPassword' => $_POST['dbPassword'],
-	'dbName' => $_POST['dbName']
+    'dbHost' => $_POST['dbHost'],
+    'dbUsername' => $_POST['dbUsername'],
+    'dbPassword' => $_POST['dbPassword'],
+    'dbName' => $_POST['dbName']
 );
 
 // Auf vorhandene Konfiguration prüfen
@@ -25,6 +25,7 @@ if (in_array($dataSetup['step'], array(1, 2))) {
         echo date('H:i:s') . ' Keine config.php!';
         exit();
     }
+    // Konfiguration einbinden
     include_once($configFilename);
     // Prüfen ob Benutzertabelle leer
     if (mysqli_query($link, "SELECT COUNT(*) > 0 AS 'userAviable' FROM `users` LIMIT 1")) {
@@ -38,103 +39,137 @@ if (in_array($dataSetup['step'], array(1, 2))) {
 <!DOCTYPE html>
 <html lang="de">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Setup Konfiguration</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Setup Konfiguration</title>
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
-	<!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
-	<!-- Bootstrap JS -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous" async></script>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous" async></script>
 </head>
 <body>
 <?php
 
 switch ($dataSetup['step']) {
-	case (0): // Willkommensseite
-		?>
-		<p>Willkommen bei SmallReply. Um mit dem Einrichten zu beginnen, benötigen wir einige Informationen zur Datenbank:</p>
-		<ul>
-			<li>Datenbank-Server</li>
-			<li>Datenbank-Benutzername</li>
-			<li>Datenbank-Passwort</li>
-			<li>Datenbank-Name</li>
-		</ul>
-		<p>Diese Informationen werden für die Erstellung der Datei <code>config.php</code> genutzt. </p>
-		<p>Mit diesem Setup wird ausserdem der erste Benutzer für den Administratoren-Zugriff erstellt.</p>
-		<a href="setup.php?step=1">Los Geht's</a>
-		<?php
-		break;
-	case (1): // Datenbank Zugangsdaten eingeben
-		?>
-		<form method="post" action="setup.php?step=2">
-			<p>Hier sollten die Zugangsdaten zu Ihrer Datenbank eigentragen werden.</p>
-			<div>
-				<label for="dbHost">Datenbank-Host</label>
-				<input id="dbHost" type="text" name="dbHost" value="localhost">
-			</div>
-			<div>
-				<label for="dbUsername">Benutzername</label>
-				<input id="dbUsername" type="text" name="dbUsername">
-			</div>
-			<div>
-				<label for="dbPassword">Passwort</label>
-				<input id="dbPassword" type="password" name="dbPassword">
-			</div>
-			<div>
-				<label for="dbName">Datenbank-Name</label>
-				<input id="dbName" type="text" name="dbName" value="smallreply">
-			</div>
-			<div>
-				<input type="submit" value="Absenden" name="submit">
-			</div>
-		</form> 
-		<?php
-		break;
-	case (2): // Datenbankverbindung überprüfen
-		// Mit der Datenbank verbinden
-		$tempLink = mysqli_connect($dataInput['dbHost'], $dataInput['dbUsername'], $dataInput['dbPassword'], $dataInput['dbName']);
+    case (0): // Willkommensseite
+        ?>
+        <p>Willkommen bei SmallReply. Um mit dem Einrichten zu beginnen, benötigen wir einige Informationen zur Datenbank:</p>
+        <ul>
+            <li>Datenbank-Server</li>
+            <li>Datenbank-Benutzername</li>
+            <li>Datenbank-Passwort</li>
+            <li>Datenbank-Name</li>
+        </ul>
+        <p>Diese Informationen werden für die Erstellung der Datei <code>config.php</code> genutzt. </p>
+        <p>Mit diesem Setup wird ausserdem der erste Benutzer für den Administratoren-Zugriff erstellt.</p>
+        <a href="setup.php?step=1">Los Geht's</a>
+        <?php
+        break;
+    case (1): // Datenbank Zugangsdaten eingeben
+        ?>
+        <form method="post" action="setup.php?step=2">
+            <p>Hier sollten die Zugangsdaten zu Ihrer Datenbank eigentragen werden.</p>
+            <div>
+                <label for="dbHost">Datenbank-Host</label>
+                <input id="dbHost" type="text" name="dbHost" value="localhost">
+            </div>
+            <div>
+                <label for="dbUsername">Benutzername</label>
+                <input id="dbUsername" type="text" name="dbUsername">
+            </div>
+            <div>
+                <label for="dbPassword">Passwort</label>
+                <input id="dbPassword" type="password" name="dbPassword">
+            </div>
+            <div>
+                <label for="dbName">Datenbank-Name</label>
+                <input id="dbName" type="text" name="dbName" value="smallreply">
+            </div>
+            <div>
+                <input type="submit" value="Absenden" name="submit">
+            </div>
+        </form> 
+        <?php
+        break;
+    case (2): // Datenbankverbindung überprüfen
+        // Mit der Datenbank verbinden
+        $tempLink = mysqli_connect($dataInput['dbHost'], $dataInput['dbUsername'], $dataInput['dbPassword'], $dataInput['dbName']);
 
-		// Verbindung überprüfen
-		if (!$tempLink) {
-			?>
-			<h1>Fehler beim Aufbau einer Datenbankverbindung</h1>
-			<p>Das bedeutet, dass der eingegebene Benutzername oder das Passwort nicht korrekt ist. Möglicherweise kann auch der Datenbankserver auf <code><?php echo $dataInput['dbHost'] ?></code> nicht erreicht werden.</p>
-			<ul>
-				<li>Sind Benutzername und Passwort korrekt?</li>
-				<li>Ist der Datenbankserver unter dem eingegebenen Hostnamen oder IP-Addresse erreichbar?</li>
-				<li>Hat der Benutzer die benötigten Rechte für die angegebene Datenbank?</li>
-			</ul>
-			<a href="setup.php?step=1">Erneut versuchen</a>
-			<?php
-			break;
-		} else {
+        // Verbindung überprüfen
+        if (!$tempLink) {
+            ?>
+            <h1>Fehler beim Aufbau einer Datenbankverbindung</h1>
+            <p>Das bedeutet, dass der eingegebene Benutzername oder das Passwort nicht korrekt ist. Möglicherweise kann auch der Datenbankserver auf <code><?php echo $dataInput['dbHost'] ?></code> nicht erreicht werden.</p>
+            <ul>
+                <li>Sind Benutzername und Passwort korrekt?</li>
+                <li>Ist der Datenbankserver unter dem eingegebenen Hostnamen oder IP-Addresse erreichbar?</li>
+                <li>Hat der Benutzer die benötigten Rechte für die angegebene Datenbank?</li>
+            </ul>
+            <a href="setup.php?step=1">Erneut versuchen</a>
+            <?php
+            break;
+        } else {
 
-			// Eingabewerte zur Konfigurationsvorlage hinzufügen
-			foreach ($dataInput as $key => $value) {
-				$search = "'" . $key . "' => ''";
-				$replace = "'" . $key . "' => '" . $value . "'";
-				$dataSetup['configTemplate'] = str_replace($search, $replace, $dataSetup['configTemplate']);
-			}
-			$dataSetup['configTemplate'][0] = '<?php /* THIS FILE WAS GENERATED BY THE SMALLREPLY FRONT-END ' . date('d.m.Y H:i:s') . ' */' . PHP_EOL;
+            // Eingabewerte zur Konfigurationsvorlage hinzufügen
+            foreach ($dataInput as $key => $value) {
+                $search = "'" . $key . "' => ''";
+                $replace = "'" . $key . "' => '" . $value . "'";
+                $dataSetup['configTemplate'] = str_replace($search, $replace, $dataSetup['configTemplate']);
+            }
+            $dataSetup['configTemplate'][0] = '<?php /* THIS FILE WAS GENERATED BY THE SMALLREPLY FRONT-END ' . date('d.m.Y H:i:s') . ' */' . PHP_EOL;
 
-			// Eingabewerte in Config.php schreiben
+            // Eingabewerte in Config.php schreiben
             file_put_contents('config.php', $dataSetup['configTemplate']);
 
             ?>
-			<p>Die Verbindung zur Datenbank konnte erfolgreich hergestellt werden</p>
+            <p>Die Verbindung zur Datenbank konnte erfolgreich hergestellt werden</p>
             <p>Die Tabellen können im nächsten Schritt automatisch erstellt werden.</p>
-			<a href="setup.php?step=3">Tabellen erstellen</a>
+            <a href="setup.php?step=3">Tabellen erstellen</a>
             <a href="setup.php?step=4">Benutzer</a>
-			<?php
-		}
+            <?php
+        }
         break;
     case (3): // Tabellen erstellen
-    case (4): // Initial Benutzer erstellen
+    case (4): // Initial Benutzerdaten eingeben
+        ?>
+        <form action="setup.php?step=5" method="POST">
+            <div>
+                <label for="username">Benutzername</label>
+                <input id="username" type="text" name="username">
+            </div>
+            <div>
+                <label for="firstname">Vorname</label>
+                <input id="firstname" type="text" name="firstname">	
+            </div>
+            <div>
+                <label for="lastname">Nachname</label>
+                <input id="lastname" type="text" name="lastname">
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email">
+            </div>
+            <div>
+                <label for="password">Passwort</label>
+                <input id="password" type="password" name="password">
+            </div>
+            </div>
+            <div>
+                <label for="passwordRepeat">Passwort wiederholen</label>
+                <input id="passwordRepeat" type="password" name="passwordRepeat">
+            </div>
+            <div>
+                <input type="submit" value="Absenden" name="submit">
+            </div>
+        </form>
+        <?php
+        break;
+    case (5): // Initial Benutzer anlegen
 }
 
 // HTML Footer
